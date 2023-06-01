01aa5d1bdf8a436a54b37eb288e9172c8c66059d
Chase Ward <chasew8095@gmail.com>
2023-05-31 22:48:26 -0500

    fresh start

--- solitare.js
+++ solitare.js
@@ -18,34 +18,3 @@ while (currentIndex != 0) {
     [deck[currentIndex], deck[randomIndex]] = [
     deck[randomIndex], deck[currentIndex]];
 }
-
-// pre-game set tableau 
-let cardClass = document.querySelectorAll('.card')
-cardClass.forEach(card => {
-    card.style.backgroundImage = 'url(/'+deck[0][1]+'/'+deck[0][0]+'.png)'
-    console.log('url(/'+deck[0][1]+'/'+deck[0][0]+'.png)')
-    card.style.backgroundRepeat = 'no-repeat'
-    card.style.borderRadius = '7%'
-    card.style.position = 'relative'
-    deck.splice(0, 1)
-})
-
-let cardId
-// get id of clicked card
-cardClass = document.getElementsByClassName("card")
-const cardPressed = e => {
-    cardId = e.target.id
-}
-for (let card of cardClass) {
-    card.addEventListener("click", cardPressed)
-}
-
-// make card follow mouse
-document.addEventListener('mousemove', function(e) {
-    let body = document.querySelector('body')
-    clickedCard = document.getElementById(cardId);
-    let left = e.offsetX;
-    let top = e.offsetY;
-    clickedCard.style.left = left + 'px';
-    clickedCard.style.top = top + 'px';
-})
